2d1465f4cd396ea3fd77c81a7703fdef95294cc8
-m <-m>
2023-03-23 15:30:57 +0500

    second version with API

--- js/api.js
+++ js/api.js
@@ -13,15 +13,64 @@ let elImgUrl = document.querySelector("[data-form-img-url]");
 let elBtn = document.querySelector("[data-add-btn]");
 let elTemplate = document.querySelector("[data-movie-template]");
 let elTemplateAbout = document.querySelector("[data-template]");
-let closeModal = document.querySelector('[data-close-modal]');
 
-elMovieBox.innerHTML = `<div class="loader">
-<span class="first"></span>
-<span class="second"></span>
-<span class="third"></span>
-</div>`;
 
-searchMovie("movie");
+loadingFirst()
+async function loadingFirst(){
+  elMovieBox.innerHTML = `<div class="inner" style="width: 200px; height: 200px;"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgb(22, 29, 37); display: block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+  <g transform="rotate(0 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(30 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(60 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(90 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(120 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(150 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(180 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(210 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(240 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(270 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(300 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+    </rect>
+  </g><g transform="rotate(330 50 50)">
+    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+      <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+    </rect>
+  </g>
+  </svg></div>`
+  await searchMovie("movie")
+}
+// searchMovie("movie");
 
 const elForm = document.querySelector("[data-input-search]");
 elForm.addEventListener("submit", (evt) => {
@@ -91,16 +140,64 @@ let aboutUl = document.querySelector("[data-ul2]");
 
 async function aboutMovie(num) {
   try {
-    aboutUl.innerHTML = `<div class="loader">
-    <span class="first"></span>
-    <span class="second"></span>
-    <span class="third"></span>
-    </div>`;
+    aboutUl.innerHTML = 
+    elMovieBox.innerHTML = `<div class="inner" style="width: 200px; height: 200px;"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: rgb(22, 29, 37); display: block;" width="200px" height="200px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+    <g transform="rotate(0 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(30 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(60 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(90 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(120 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(150 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(180 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(210 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(240 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(270 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(300 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(330 50 50)">
+      <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#aaaaaa">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animate>
+      </rect>
+    </g>
+    </svg></div>`;
     let response = await fetch(`${apiKey}&i=${num}`);
     let result = await response.json();
     // renderMovie(result)
 
     aboutUl.innerHTML = "";
+    console.log(result);
     // let movie =    // movies.find((a) => a.id == +num);
     aboutUl.append(createDiv(result));
   } catch (error) {
